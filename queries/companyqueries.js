087894ff5e376519b1a42987f2b71baf5a1f4923
treycottingham <marchant.cottingham@gmail.com>
2018-06-12 11:09:55 -0600

    queries

--- queries/companyqueries.js
+++ queries/companyqueries.js
@@ -5,10 +5,18 @@ module.exports = {
     return database("company_tb")
     },
     create(company) {
-        return database("company_tb")
-            .insert(company)
-            .returning("*")
-            .then(record => record[0])
+        return database("company_tb").insert(company,'*')
+            // .insert(company)
+            // .returning("*")
+            // .then(record => record[0])
+    },
+    update(id, concert) {
+        return database('company_tb').where('id', id).update(company, '*')
+    },
+    delete(id) {
+        return database('company_tb').where('id', id).delete()
+    },
+    read(id) {
+        return database('company_tb').where('id', id).first()
     }
-
 }
