314b156a3746ba0695f920d0c4284acb959e6c11
Administrator <adriandlai@gmail.com>
2018-06-14 15:01:01 -0600

    got read route fixed to upate by company_id

--- queries/contactqueries.js
+++ queries/contactqueries.js
@@ -8,12 +8,12 @@ module.exports = {
         return database("contact_tb").insert(contact,'*').then(contact => contact[0])
     },
     update(id, contact) {
-        return database('contact_tb').where('company_tb_id', id).update(contact, '*').then(contact => contact[0])
+        return database('contact_tb').where('id', id).update(contact, '*').then(contact => contact[0])
     },
     delete(id) {
         return database('contact_tb').where('id', id).delete()
     },
     read(id) {
-        return database('contact_tb').where('id', id).first()
+        return database('contact_tb').where('company_tb_id', id)
     }
 }
